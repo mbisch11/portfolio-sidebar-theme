e51f47bbf1fe603d114a9a74ef62f325686f2c5d
mbisch11 <mbisch11@gmail.com>
2025-04-15 12:59:51 -0400

    Adding page template and planning the layout of the page

--- page-temp.js
+++ page-temp.js
@@ -6,16 +6,10 @@ import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 
-/**
- * `portfolio-sidebar-theme`
- * 
- * @demo index.html
- * @element portfolio-sidebar-theme
- */
-export class PortfolioSidebarTheme extends DDDSuper(I18NMixin(LitElement)) {
+export class TempPage extends DDDSuper(I18NMixin(LitElement)) {
 
   static get tag() {
-    return "portfolio-sidebar-theme";
+    return "page-temp";
   }
 
   constructor() {
@@ -35,18 +29,9 @@ export class PortfolioSidebarTheme extends DDDSuper(I18NMixin(LitElement)) {
   static get styles() {
     return [super.styles,
     css`
-      :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-      }
       .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      h3 span {
-        font-size: var(--portfolio-sidebar-theme-label-font-size, var(--ddd-font-size-s));
+        height: 100vh;
+        width: 100vw;
       }
     `];
   }
@@ -68,4 +53,4 @@ export class PortfolioSidebarTheme extends DDDSuper(I18NMixin(LitElement)) {
   }
 }
 
-globalThis.customElements.define(PortfolioSidebarTheme.tag, PortfolioSidebarTheme);
+globalThis.customElements.define(TempPage.tag, TempPage);
